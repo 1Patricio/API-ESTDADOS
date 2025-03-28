1f3c6fb9ea9d322afbe21bfd2b36d5791e821a8a
1Patricio <andersongpatricio@hotmail.com>
2025-03-27 21:56:56 -0300

    Parte da segunda atividade

--- AP1.java
+++ AP1.java
@@ -1,8 +1,6 @@
 package um;
 
-import javax.swing.*;
-import java.text.DecimalFormat;
-import java.time.format.DecimalStyle;
+import java.util.Arrays;
 
 public class AP1 {
     public static void main(String[] args) {
@@ -71,6 +69,51 @@ public class AP1 {
                     System.out.println((i + 1) + "ºLugar: " + alunos[i] + " com a nota: " + notas[i]);
                 }
             }
+        }  // Atividade 01
+
+        class Matriz {
+            public Double[][] temperaturas;
+            public Double temperatura;
+            public String[] cidades;
+            public String cidade;
+
+            public Matriz(int numeroCidades, int qtdTemperaturas) {
+                this.cidades = new String[numeroCidades];
+                this.temperaturas = new Double[numeroCidades][qtdTemperaturas];
+            }
+
+            public void adicionarCidadeETempeturatura(){
+                int min = -5;
+                int max = 42;
+
+                for (int i = 0; i < cidades.length; i++) {
+                    this.cidades[i] = "Cidade " + i;
+
+                    for (int j = 0; j < temperaturas[i].length; j++) {
+
+                        this.temperatura = Math.random() * (max - min) + min;
+                        Double temperaturaArredondada = (double)Math.round(this.temperatura * 100.0) / 100.0;
+                        this.temperaturas[i][j] = temperaturaArredondada;;
+                    }
+                }
+                // Exibir os dados corretamente
+                for (int i = 0; i < cidades.length; i++) {
+                    System.out.println(cidades[i] + " - Temperaturas: " + Arrays.toString(temperaturas[i]));
+                }
+            }
+            public void mediaTemperaturaPorCidade(){
+                Double media = 0.00;
+                for (int i = 0; i < cidades.length; i++) {
+                    for (int j = 0; j < temperaturas[i].length; j++) {
+                        media += temperaturas[i][j];
+                    }
+                    media = media / temperaturas[i].length;
+                    Double mediaArredondada = (double)Math.round(media * 100.0) / 100.0;
+                    System.out.println("Média de temperatura da " + cidades[i] + ":");
+                    System.out.println(mediaArredondada + "ºC");
+                    System.out.println();
+                }
+            }
         }
 
         Vetor vetor = new Vetor(30);
@@ -78,5 +121,14 @@ public class AP1 {
         vetor.calcularMedia();
         vetor.alunoAcimaMedia();
         vetor.melhoresTurma();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("=== ATIVIDADE 02 ===");
+
+        Matriz matriz = new Matriz(5, 7);
+        matriz.adicionarCidadeETempeturatura();
+        matriz.mediaTemperaturaPorCidade();
     }
 }
